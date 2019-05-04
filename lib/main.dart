f9f2d1016c22c2e879f661b4d97b3408c50d419f
Arunkumar Vasudevan <arunkumarv31@gmail.com>
2019-05-04 14:28:54 +0530

    Created flutter project and added stateless, stateful and state classes

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+/* StatelessWidget */
+class MyApp extends StatelessWidget {
+  //Stateless widgets are immutable, meaning that their properties can’t change—all values are final.
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
+    return MaterialApp(
+        title: 'Welcome to Flutter',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Welcome to flutter')
+          ),
+          body: Center(
+            child: RandomWords()
+          ),
+        ));
+  }
+}
+
+/* 
+   Implementing a stateful widget requires at least two classes: 
+    1) a StatefulWidget class that creates an instance of 
+    2) a State class. 
+        The StatefulWidget class is, itself, immutable, 
+        but the State class persists over the lifetime of the widget.
+*/
+
+/* Stateful widget */
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+/* State class */
+class RandomWordsState extends State<RandomWords> {
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
