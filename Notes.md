fad38caea165026e1a6b65897f1c9297b2099af3
Arunkumar Vasudevan <arunkumarv31@gmail.com>
2019-05-06 22:14:43 +0530

    Added notes and tabs tut finished

---
+++ Notes.md
@@ -0,0 +1,10 @@
+* Immutable class - Properties of the class cannot change. All the values are final
+* StatelessWidget and StatefulWidget is Immutable, But State class is Mutable.
+* StatelessWidget: It has a build method which describes the user interface represented by this widget
+* StatefulWidget: It creates an Instance of State class.
+* State: persists over the lifetime of the widget.
+* Prefixing an identifier with an underscore enforces privacy in the Dart language.
+* Text Widget is a child widget and Directionality is a container widget.
+* Build method expects at least one container widget.
+* _Why not everything stateful ?_
+* Whenever a tab is associated with a stateful widget it regenerates the widget everytime the tab is selected.
--- lib/tabstut.dart
+++ lib/tabstut.dart
@@ -4,52 +4,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("AppBar With tabs"),
-            bottom: TabBar(
-              tabs: <Widget>[
-                Tab(icon: Icon(Icons.android)), 
-                Tab(icon:Icon(Icons.cloud_download))
-              ],
-            )
-          ),
-          body: TabBarView(
-            children: <Widget>[
-              AndroidAction(),//Text("Android Selected"),
-              Text("Cloud control Selected")
-            ],
-          )
-        )
-      )
-    );
+        home: DefaultTabController(
+            length: 2,
+            child: Scaffold(
+                appBar: AppBar(
+                    title: Text("AppBar With tabs"),
+                    bottom: TabBar(
+                      tabs: <Widget>[
+                        Tab(icon: Icon(Icons.android)),
+                        Tab(icon: Icon(Icons.cloud_download))
+                      ],
+                    )),
+                body: TabBarView(
+                  children: <Widget>[
+                    AndroidAction(), //Text("Android Selected"),
+                    CloudAction()//Text("Cloud control Selected")
+                  ],
+                ))));
   }
 }
 
 class AndroidAction extends StatefulWidget {
-
   @override
-  State<StatefulWidget> createState(){
+  State<StatefulWidget> createState() {
     return AndroidActionApp();
   }
 }
 
-class AndroidActionApp extends State<AndroidAction>{
-
+class AndroidActionApp extends State<AndroidAction> {
   int counter = 0;
   bool tickmark = false;
 
   @override
-  Widget build ( BuildContext context ){
-
-    return Column (
+  Widget build(BuildContext context) {
+    return Column(
       children: <Widget>[
         Text("Counter value = $counter"),
         Checkbox(
-          value:tickmark,
-          onChanged: (bool newValue){
+          value: tickmark,
+          onChanged: (bool newValue) {
             setState(() {
               counter++;
               tickmark = newValue;
@@ -58,5 +51,15 @@ class AndroidActionApp extends State<AndroidAction>{
         )
       ],
     );
+  }
+}
+
+class CloudAction extends StatelessWidget {
+  
+  int counter = 0;
+
+  Widget build(BuildContext context) {
+    counter++;
+    return Text("Counter value = $counter");
   }
 }
