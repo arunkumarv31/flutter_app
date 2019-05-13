7b8dd1d53834270106c59a3e48b48a010798d2c0
Arunkumar Vasudevan <arunkumarv31@gmail.com>
2019-05-13 17:14:37 +0530

    product details stuff

--- lib/udemy/pages/product.dart
+++ lib/udemy/pages/product.dart
@@ -8,11 +8,25 @@ class ProductPage extends StatelessWidget {
         title: Text("Product Details"),
       ),
       body: Column(
+        //mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Text('Details'),
-          RaisedButton( child: Text('BACK'), onPressed: () => Navigator.pop(context),)
+          Image.asset('assets/food.jpg'),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Details'),
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              color: Theme.of(context).accentColor,
+              child: Text('BACK'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          )
         ],
-      )
+      ),
+
       /* Center(
         child: Text('On the Product Page'),
       ), */
